85830baf6d3531a8ed7ba5ed70d20f5c0329af24
malli031998 <vvdmallikarjunareddy.com>
2024-06-23 13:00:47 +0530

    first commit in facalty-service

---
+++ src/test/java/com/example/facalty_service/FacaltyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facalty_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacaltyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
